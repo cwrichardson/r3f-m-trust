b52cdbdaeccc4405b65ebe9d08243aa0df27a77e
Christopher Richardson <cwr@Christophers-MacBook-Pro.local>
2024-10-28 07:50:13 -0400

    UVs with normalized position

--- src/glsl/fragment.js
+++ src/glsl/fragment.js
@@ -3,7 +3,8 @@ export const fragment = /* glsl */ `
     varying vec3 vPosition;
 
     void main() {
+        vec2 newUV = vPosition.xy/vec2(480., 820.);
         gl_FragColor = vec4(1., 0., 0., 1.);
-        gl_FragColor = vec4(vPosition, 1.);
+        gl_FragColor = vec4(newUV, 0., 1.);
     }
 `;
