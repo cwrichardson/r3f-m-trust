2954473bdc46b484f178bcf5ab195e57305d747c
Christopher Richardson <cwr@Christophers-MacBook-Pro.local>
2024-10-28 09:22:03 -0400

    Discard black particles

--- src/glsl/fragment.js
+++ src/glsl/fragment.js
@@ -6,6 +6,7 @@ export const fragment = /* glsl */ `
         // vec2 newUV = vPosition.xy/vec2(480., 820.) + vec2(0.5);
         // gl_FragColor = vec4(1., 0., 0., 1.);
         vec4 texture = texture2D(uTexture, vUv);
+        if (texture.r < 0.1 && texture.g<0.1 && texture.b<0.1) discard;
         // gl_FragColor = vec4(vUv, 0., 1.);
         // gl_FragColor = vec4(1., 1., 1., 1.);
         gl_FragColor = texture;
