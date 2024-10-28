3b0a4b7dadcc03026b18087596bd41a4a83122fd
Christopher Richardson <cwr@Christophers-MacBook-Pro.local>
2024-10-28 07:46:51 -0400

    Use position for uvs.

--- src/glsl/fragment.js
+++ src/glsl/fragment.js
@@ -1,7 +1,9 @@
 export const fragment = /* glsl */ `
     varying vec2 vUv;
+    varying vec3 vPosition;
 
     void main() {
         gl_FragColor = vec4(1., 0., 0., 1.);
+        gl_FragColor = vec4(vPosition, 1.);
     }
 `;
--- src/glsl/vertex.js
+++ src/glsl/vertex.js
@@ -1,11 +1,14 @@
 export const vertex = /* glsl */ `
     uniform float uProgress;
+    uniform sampler2D uTexture;
     uniform float uTime;
     attribute vec2 aCoords;
     varying vec2 vUv;
+    varying vec3 vPosition;
 
     void main() {
         vUv = aCoords;
+        vPosition = position;
 
         vec4 mvPosition = modelViewMatrix * vec4( position, 1. );
         // start with big particles; give them some perspective
