57e9bb52cdd0436468232e8af55c2a36e54c8128
Christopher Richardson <cwr@Christophers-MacBook-Pro.local>
2024-10-28 09:08:24 -0400

    switch to white

--- src/glsl/fragment.js
+++ src/glsl/fragment.js
@@ -4,9 +4,10 @@ export const fragment = /* glsl */ `
 
     void main() {
         // vec2 newUV = vPosition.xy/vec2(480., 820.) + vec2(0.5);
-        gl_FragColor = vec4(1., 0., 0., 1.);
+        // gl_FragColor = vec4(1., 0., 0., 1.);
         vec4 texture = texture2D(uTexture, vUv);
         // gl_FragColor = vec4(vUv, 0., 1.);
+        gl_FragColor = vec4(1., 1., 1., 1.);
         // gl_FragColor = texture;
     }
 `;
